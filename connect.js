05b5764e3f4ba18bbe9b05e0be271e16f4523daa
Nishirouille <145865731+Nishirouille@users.noreply.github.com>
2026-05-03 18:34:13 +0200

    Refactor login and signup event listeners

--- connect.js
+++ connect.js
@@ -1,11 +1,3 @@
-const SUPABASE_URL = "https://hihjodtikgwomspiadea.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpaGpvZHRpa2d3b21zcGlhZGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2ODEzMDQsImV4cCI6MjA5MzI1NzMwNH0.tU21-ozXiYZsDhxTYT0hBGJxuDQnC9KQlqlT25GB1Ls";
-
-const client = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
-
 document.getElementById("loginForm")
 .addEventListener("submit", async (e) => {
     e.preventDefault();
